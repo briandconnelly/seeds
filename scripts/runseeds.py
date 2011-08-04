0c6e644a90a9ccddbe2bfd45829f8c919fa0a152
Brian Connelly <bdc@msu.edu>
2011-08-04 16:08:39 -0400

    1.0.9 release

--- scripts/runseeds.py
+++ scripts/runseeds.py
@@ -9,7 +9,7 @@ or system-wide repository of plugins.
 """
 
 __author__ = "Brian Connelly <bdc@msu.edu>"
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 __credits__ = "Brian Connelly"
 
 import seeds as S
